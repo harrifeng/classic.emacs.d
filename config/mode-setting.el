7d4f97563fa7a1c8c01b645973c4151030c24c34
hfeng.nish <harrifeng@gmail.com>
2013-08-06 10:56:34 +0800

    ipython configuration for windows-nt

--- config/mode-setting.el
+++ config/mode-setting.el
@@ -12,3 +12,18 @@
 
 ;; makefile mode---------->>
 (add-to-list 'auto-mode-alist '("\\.mak\\'" . makefile-mode))
+
+;; python mode------------>>
+(require 'python)
+(setq
+ python-shell-interpreter "python.exe"
+ python-shell-interpreter-args (concat "-i " (concat my-python-script-path
+                                                     "ipython-script.py"))
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+ "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+ "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
--- config/path-setting.el
+++ config/path-setting.el
@@ -4,10 +4,20 @@
     (defconst my-dropbox-dev "C:/Dropbox/Development/")
   (defconst my-dropbox-dev "d:/Dropbox/Development/"))
 
+(if (file-exists-p "c:/python26")
+    (defconst my-python-path "c:/python26/")
+  (defconst my-python-path "d:/python26/"))
+
+(defconst my-python-script-path
+  (concat my-python-path "Scripts/"))
+
 (defconst my-git-path (concat my-dropbox-dev "PortableGit/bin/"))
+
 (setenv "PATH"
 	(concat
 	 my-git-path ";"
+	 my-python-path ";"
+	 my-python-script-path ";"
 	 (getenv "PATH")))
 ;; exec-path based on path, can work on all system if path is
 ;; set successfully
