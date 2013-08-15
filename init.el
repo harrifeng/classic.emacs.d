e3a1894b445cde366fe0d42574d275d288b12091
hfeng-air <harrifeng@gmail.com>
2013-08-15 17:42:33 +0800

    el-get need git in exec path

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 
 (require 'basic-setting)
 (require 'helpfunc-setting)
+(require 'path-setting)
 (require 'systype-setting)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; path is used by others ;;
@@ -21,6 +22,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; keybind is the last to load ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'path-setting)
 (require 'keybind-setting)
 
