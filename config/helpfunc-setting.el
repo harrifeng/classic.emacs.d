8a44e1bc7d2b846aaa2836c149b7ad05d0359385
hfeng.nish <harrifeng@gmail.com>
2013-08-26 11:48:44 +0800

    "add some improve to master"

--- config/helpfunc-setting.el
+++ config/helpfunc-setting.el
@@ -1,5 +1,16 @@
 (provide 'helpfunc-setting)
 
+(defun whack-whitespace (arg)
+  "Delete all white space from point to the next word.  With prefix ARG
+    delete across newlines as well.  The only danger in this is that you
+    don't have to actually be at the end of a word to make it work.  It
+    skips over to the next whitespace and then whacks it all to the next
+    word."
+  (interactive "P")
+  (let ((regexp (if arg "[ \t\n]+" "[ \t]+")))
+    (re-search-forward regexp nil t)
+    (replace-match "" nil nil)))
+
 (defun shell-mode-auto-rename-buffer (text)
   (if (eq major-mode 'shell-mode)
       (rename-buffer  (concat "*Shell: "
