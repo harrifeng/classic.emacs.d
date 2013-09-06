2034c96a5fff3f3f8509d2193da333a89fb1421f
hfeng.nish <harrifeng@gmail.com>
2013-09-06 13:38:46 +0800

    new sytle el-setting is enough

--- config/el-setting.el
+++ config/el-setting.el
@@ -1,31 +1,40 @@
 (provide 'el-setting)
 
+;; ;; [A]nything-config--------------------------------------------------------->>
+;; (require 'anything-config)
+;; (global-set-key (kbd "C-x b")
+;;   (lambda() (interactive)
+;;     (anything
+;;      :prompt "Switch to: "
+;;      :candidate-number-limit 10                 ;; up to 10 of each
+;;      :sources
+;;      '( anything-c-source-buffers               ;; buffers
+;;         anything-c-source-recentf               ;; recent files
+;;         anything-c-source-bookmarks             ;; bookmarks
+;;         anything-c-source-files-in-current-dir+ ;; current dir
+;;         anything-c-source-locate))))            ;; use 'locate'
+
 ;; ;; [A]uto-complete-------------------------------------------------------->>
 ;; (require 'auto-complete-config)
 ;; (ac-config-default)
-;; ;;------------------------------------------------------------------------||
 
 ;; ;; [C]olor theme autoload[package:sublime-themes]------------------------->>
 ;; (load-theme 'hickey t)
-;; ;;------------------------------------------------------------------------||
 
 ;; ;; [G]rep-a-lot----------------------------------------------------------->>
 ;; (require 'grep-a-lot)
 ;; (grep-a-lot-setup-keys)
-;; ;;------------------------------------------------------------------------||
 
 ;; ;; [J]edi plugin---------------------------------------------------------->>
 ;; (add-hook 'python-mode-hook 'jedi:ac-setup)
 ;; (setq jedi:setup-keys t)
-;; ;;------------------------------------------------------------------------||
 
-;; ;; [H]ighlight-symbol---------------------------------------------------------->>
+;; ;; [H]ighlight-symbol----------------------------------------------------->>
 ;; (setq highlight-symbol-mode t)
 ;; (global-set-key [(control f3)] 'highlight-symbol-at-point)
 ;; (global-set-key [f3] 'highlight-symbol-next)
 ;; (global-set-key [(shift f3)] 'highlight-symbol-prev)
 ;; (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
-;; ;;------------------------------------------------------------------------||
 
 ;; ;; [M]util-web-mode------------------------------------------------------->>
 ;; (require 'multi-web-mode)
@@ -35,17 +44,14 @@
 ;;                   (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
 ;; (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
 ;; (multi-web-global-mode 1)
-;; ;;------------------------------------------------------------------------||
 
 ;; ;; [Y]asnippet------------------------------------------------------------>>
 ;; (require 'yasnippet)
 ;; (yas/initialize)
 ;; (yas/load-directory "~/.emacs.d/path-to-yasnippet/snippets/")
-;; ;;------------------------------------------------------------------------||
 
 ;; ;; [Y]asnippet-bundle----------------------------------------------------->>
 ;; (require 'yasnippet-bundle)
-;; ;;------------------------------------------------------------------------||
 
 ;; ;; [Y]asnippet & [Y]asnippet bundle share following setting--------------->>
 ;; ;; modify conflict with org-mode
@@ -68,4 +74,3 @@
 ;;         yas/x-prompt
 ;;         yas/completing-prompt
 ;;         yas/no-prompt))
-;; ;;------------------------------------------------------------------------||
