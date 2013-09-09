8db024f89ae85522c8f1924c631e7aa15fb03dbc
hfeng-air <harrifeng@gmail.com>
2013-09-09 22:09:50 +0800

    this is it, use the package to install missing plugins, and then configure them. Here we exchange anything and anything-config to make it work. It seems that the anything can't be the first one to install

--- config/el-setting.el
+++ config/el-setting.el
@@ -1,94 +1,94 @@
 (provide 'el-setting)
 
-;; (package-initialize)
-;; ;; check if the packages is installed; if not, install it.
-;; (mapc
-;;  (lambda (package)
-;;    (or (package-installed-p package)
-;;        (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-;;            (package-install package))))
-;;  '(
-;;    anything
-;;    anything-config
-;;    auto-complete
-;;    grep-a-lot
-;;    highlight-symbol
-;;    jedi
-;;    multi-web-mode
-;;    magit
-;;    rainbow-mode
-;;    sublime-themes
-;;    yasnippet dropdown-list ;; dropdown-list is needed by yasnippet
-;;    ))
-;;
-;; ;; [A]nything-config && [A]nything---------------------------------------->>
-;; (require 'anything-config)
-;; (global-set-key (kbd "C-x b")
-;;   (lambda() (interactive)
-;;     (anything
-;;      :prompt "Switch to: "
-;;      :candidate-number-limit 10                 ;; up to 10 of each
-;;      :sources
-;;      '( anything-c-source-buffers               ;; buffers
-;;         anything-c-source-recentf               ;; recent files
-;;         anything-c-source-bookmarks             ;; bookmarks
-;;         anything-c-source-files-in-current-dir+ ;; current dir
-;;         anything-c-source-locate))))            ;; use 'locate'
-;;
-;; ;; [A]uto-complete-------------------------------------------------------->>
-;; (require 'auto-complete-config)
-;; (ac-config-default)
-;;
-;; ;; [G]rep-a-lot----------------------------------------------------------->>
-;; (require 'grep-a-lot)
-;; (grep-a-lot-setup-keys)
-;;
-;; ;; [H]ighlight-symbol----------------------------------------------------->>
-;; (setq highlight-symbol-mode t)
-;; (global-set-key [(control f3)] 'highlight-symbol-at-point)
-;; (global-set-key [f3] 'highlight-symbol-next)
-;; (global-set-key [(shift f3)] 'highlight-symbol-prev)
-;; (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
-;;
-;; ;; [J]edi plugin---------------------------------------------------------->>
-;; (add-hook 'python-mode-hook 'jedi:ac-setup)
-;; (setq jedi:setup-keys t)
-;;
-;;
-;; ;; [M]agit---------------------------------------------------------------->>
-;; ;; Nothing to config now
-;;
-;; ;; [M]util-web-mode------------------------------------------------------->>
-;; (require 'multi-web-mode)
-;; (setq mweb-default-major-mode 'html-mode)
-;; (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-;;                   (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-;;                   (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-;; (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
-;; (multi-web-global-mode 1)
-;;
-;; ;; [R]ainbow-mode--------------------------------------------------------->>
-;; (require 'rainbow-mode)
-;; (dolist (hook '(css-mode-hook
-;;                 html-mode-hook))
-;;   (add-hook hook (lambda () (rainbow-mode t))))
-;;
-;;
-;; ;; [S]ublime-themes]------------------------------------------------------>>
-;; (load-theme 'hickey t)
-;;
-;; ;; [Y]asnippet------------------------------------------------------------>>
-;; (require 'yasnippet)
-;; (yas-global-mode 1)
-;; (setq yas-snippet-dirs
-;;       '(
-;;         (concat my-emacs-path "snippets")
-;;         ))
-;; ;; [d]ropdown-------------------------------------------<<
-;; (require 'dropdown-list)
-;; (setq yas/prompt-functions
-;;       '(yas/dropdown-prompt
-;;         yas/ido-prompt
-;;         yas/x-prompt
-;;         yas/completing-prompt
-;;         yas/no-prompt))
+(package-initialize)
+;; check if the packages is installed; if not, install it.
+(mapc
+ (lambda (package)
+   (or (package-installed-p package)
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package))))
+ '(
+   anything-config
+   anything
+   auto-complete
+   grep-a-lot
+   highlight-symbol
+   jedi
+   multi-web-mode
+   magit
+   rainbow-mode
+   sublime-themes
+   yasnippet dropdown-list ;; dropdown-list is needed by yasnippet
+   ))
+
+;; [A]nything-config && [A]nything---------------------------------------->>
+(require 'anything-config)
+(global-set-key (kbd "C-x b")
+  (lambda() (interactive)
+    (anything
+     :prompt "Switch to: "
+     :candidate-number-limit 10                 ;; up to 10 of each
+     :sources
+     '( anything-c-source-buffers               ;; buffers
+        anything-c-source-recentf               ;; recent files
+        anything-c-source-bookmarks             ;; bookmarks
+        anything-c-source-files-in-current-dir+ ;; current dir
+        anything-c-source-locate))))            ;; use 'locate'
+
+;; [A]uto-complete-------------------------------------------------------->>
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; [G]rep-a-lot----------------------------------------------------------->>
+(require 'grep-a-lot)
+(grep-a-lot-setup-keys)
+
+;; [H]ighlight-symbol----------------------------------------------------->>
+(setq highlight-symbol-mode t)
+(global-set-key [(control f3)] 'highlight-symbol-at-point)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+
+;; [J]edi plugin---------------------------------------------------------->>
+(add-hook 'python-mode-hook 'jedi:ac-setup)
+(setq jedi:setup-keys t)
+
+
+;; [M]agit---------------------------------------------------------------->>
+;; Nothing to config now
+
+;; [M]util-web-mode------------------------------------------------------->>
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
+
+;; [R]ainbow-mode--------------------------------------------------------->>
+(require 'rainbow-mode)
+(dolist (hook '(css-mode-hook
+                html-mode-hook))
+  (add-hook hook (lambda () (rainbow-mode t))))
+
+
+;; [S]ublime-themes]------------------------------------------------------>>
+(load-theme 'hickey t)
+
+;; [Y]asnippet------------------------------------------------------------>>
+(require 'yasnippet)
+(yas-global-mode 1)
+(setq yas-snippet-dirs
+      '(
+        (concat my-emacs-path "snippets")
+        ))
+;; [d]ropdown-------------------------------------------<<
+(require 'dropdown-list)
+(setq yas-prompt-functions
+      '(yas/dropdown-prompt
+        yas/ido-prompt
+        yas/x-prompt
+        yas/completing-prompt
+        yas/no-prompt))
