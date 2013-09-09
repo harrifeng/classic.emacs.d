f286fa4da221c560ec4c2bf9fdd36680c84dfe0f
hfeng.nish <harrifeng@gmail.com>
2013-09-09 12:32:45 +0800

    nt path improving

--- config/sub/sub-nt-path.el
+++ config/sub/sub-nt-path.el
@@ -4,20 +4,26 @@
     (defconst my-dropbox-dev "C:/Dropbox/Development/")
   (defconst my-dropbox-dev "d:/Dropbox/Development/"))
 
-(if (file-exists-p "c:/python26")
-    (defconst my-python-path "c:/python26/")
-  (defconst my-python-path "d:/python26/"))
+(cond
+ ((file-exists-p "c:/python27")
+  (defconst my-python-path "c:/python27/"))
+ ((file-exists-p "d:/python27")
+  (defconst my-python-path "d:/python27/"))
+ ((file-exists-p "c:/python26")
+  (defconst my-python-path "c:/python26/"))
+ ((file-exists-p "d:/python26")
+  (defconst my-python-path "d:/python26/")))
 
 (defconst my-python-script-path
   (concat my-python-path "Scripts/"))
 
 (defconst my-git-path (concat my-dropbox-dev "PortableGit/bin/"))
-(defconst my-mingw-path (concat my-dropbox-dev "PortableMingw/bin/"))
+(defconst my-tool-path (concat my-dropbox-dev "Tools/"))
 
 (setenv "PATH"
-	(concat
-	 my-mingw-path ";"
-	 my-git-path ";"
-	 my-python-path ";"
-	 my-python-script-path ";"
-	 (getenv "PATH")))
+        (concat
+         my-tool-path ";"
+         my-git-path ";"
+         my-python-path ";"
+         my-python-script-path ";"
+         (getenv "PATH")))
