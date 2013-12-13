02f541bd1304485bfee5439ce5494143e21ea208
unknown <hfeng@sh-rd-hfeng.apac.corp.natinst.com>
2013-12-13 19:09:07 +0800

    new dropbox dev

--- config/sub/sub-nt-path.el
+++ config/sub/sub-nt-path.el
@@ -5,6 +5,8 @@
   (defconst my-dropbox-dev "d:/Dropbox/Development/"))
 
 (cond
+ ((file-exists-p (concat my-dropbox-dev "Python27/App/"))
+  (defconst my-python-path (concat my-dropbox-dev "Python27/App/")))
  ((file-exists-p "c:/python27")
   (defconst my-python-path "c:/python27/"))
  ((file-exists-p "d:/python27")
@@ -19,6 +21,8 @@
 
 (defconst my-git-path (concat my-dropbox-dev "PortableGit/bin/"))
 (defconst my-tool-path (concat my-dropbox-dev "Tools/"))
+(defconst my-tcc-path (concat my-dropbox-dev "tcc/"))
+(defconst my-mingw-path (concat my-dropbox-dev "MingW/bin/"))
 
 (setenv "PATH"
         (concat
@@ -26,4 +30,6 @@
          my-git-path ";"
          my-python-path ";"
          my-python-script-path ";"
+         my-tcc-path ";"
+         my-mingw-path ";"
          (getenv "PATH")))
