be8fff7d72ef2f9c6365ab88a992b87b9e2cc6ee
unknown <hfeng@sh-rd-hfeng.apac.corp.natinst.com>
2013-12-25 11:15:22 +0800

    give up dropbox, use devbox instead

--- config/sub/sub-nt-path.el
+++ config/sub/sub-nt-path.el
@@ -1,29 +1,23 @@
 (provide 'sub-nt-path)
 
-(if (file-exists-p "c:/Dropbox")
-    (defconst my-dropbox-dev "C:/Dropbox/Development/")
-  (defconst my-dropbox-dev "d:/Dropbox/Development/"))
+(defconst my-dev "d:/DevBox/")
 
-(cond
- ((file-exists-p (concat my-dropbox-dev "Python27/App/"))
-  (defconst my-python-path (concat my-dropbox-dev "Python27/App/")))
- ((file-exists-p "c:/python27")
-  (defconst my-python-path "c:/python27/"))
- ((file-exists-p "d:/python27")
-  (defconst my-python-path "d:/python27/"))
- ((file-exists-p "c:/python26")
-  (defconst my-python-path "c:/python26/"))
- ((file-exists-p "d:/python26")
-  (defconst my-python-path "d:/python26/")))
-
+(defconst my-python-path
+  (concat my-dev "Python/App/"))
 (defconst my-python-script-path
   (concat my-python-path "Scripts/"))
 
-(defconst my-git-path (concat my-dropbox-dev "PortableGit/bin/"))
-(defconst my-tool-path (concat my-dropbox-dev "Tools/"))
-(defconst my-tcc-path (concat my-dropbox-dev "tcc/"))
-(defconst my-mingw-path (concat my-dropbox-dev "MingW/bin/"))
-(defconst my-java-path (concat my-dropbox-dev "java7/bin/"))
+(defconst my-git-path
+  (concat my-dev "Git/bin/"))
+
+(defconst my-tool-path
+  (concat my-dev "Tools/"))
+
+(defconst my-java-path
+  (concat my-dev "Java/bin/"))
+
+(defconst my-graphviz-path
+  (concat my-dev "Graphviz/bin/"))
 
 (setenv "PATH"
         (concat
@@ -31,7 +25,6 @@
          my-git-path ";"
          my-python-path ";"
          my-python-script-path ";"
-         my-tcc-path ";"
-         my-mingw-path ";"
          my-java-path ";"
+         my-graphviz-path ";"
          (getenv "PATH")))
