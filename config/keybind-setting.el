2931d389e2b6e20bb4ea82a9735f5d47c48d4f82
Harri Feng <harrifeng@gmail.com>
2014-09-13 20:40:09 +0800

    use m-p for query-replace

--- config/keybind-setting.el
+++ config/keybind-setting.el
@@ -18,3 +18,4 @@
 (global-set-key (kbd "M-;")          'qiang-comment-dwim-line)
 (global-set-key (kbd "M-/")          'hippie-expand)
 (global-set-key (kbd "C-j")          'newline-and-indent)
+(global-set-key (kbd "M-p")          'query-replace)
