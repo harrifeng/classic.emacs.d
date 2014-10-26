4a01349f27d5134aacbab8d3485b44c581e7d56e
hfeng <haoran.feng@sap.com>
2014-10-26 04:49:42 -0700

    you should (setq sml/no-confirm-load-theme t) before any config for sml

--- config/el-setting.el
+++ config/el-setting.el
@@ -121,6 +121,7 @@
 
 ;; [S]mart-mode-line------------------------------------------------------>>
 (require 'smart-mode-line)
+(setq sml/no-confirm-load-theme t)
 (sml/setup)
 (sml/apply-theme 'powerline)
 
