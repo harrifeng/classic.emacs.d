a30f5b8a4933d8822364aa7a155399aae8f297c6
harri feng <harrifeng@gmail.com>
2015-05-17 20:27:53 +0800

    Use Moba can use Emacs, but input method of windows can not access to this Emacs, use m-x set-input-method, then select eim-py to use input method this is only needed for moba

--- config/sub/sub-linux-path.el
+++ config/sub/sub-linux-path.el
@@ -12,3 +12,23 @@
 	 "/usr/sbin"      ":"
 	 (getenv "PATH")
 	 ))
+
+;; (add-to-list 'load-path "~/.emacs.d/config/sub/eim")
+(add-to-list 'load-path (concat my-lisps-path "sub/eim"))
+(autoload 'eim-use-package "eim" "Another emacs input method")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; if Tooltip is not ok, uncomment following sentence. ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;(setq eim-use-tooltip nil)
+
+(register-input-method
+ "eim-wb" "euc-cn" 'eim-use-package
+ "五笔" "汉字五笔输入法" "wb.txt")
+(register-input-method
+ "eim-py" "euc-cn" 'eim-use-package
+ "拼音" "汉字拼音输入法" "py.txt")
+
+;; `;` use English temporarily
+(require 'eim-extra)
+(global-set-key ";" 'eim-insert-ascii)
