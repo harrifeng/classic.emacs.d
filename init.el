01eb59445f2bb6e45ce704c022d0008ab4be2b6f
harri feng <harrifeng@gmail.com>
2015-06-06 22:35:21 +0800

    Emacs org export too many files issue fix

--- init.el
+++ init.el
@@ -5,22 +5,23 @@
 (add-to-list 'load-path my-lisps-path)
 
 (require 'basic-setting)
-(require 'helpfunc-setting)
-(require 'systype-setting)
-(require 'path-setting)
+;(require 'helpfunc-setting)
+;(require 'systype-setting)
+;(require 'path-setting)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; path is used by others ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'build-in-setting)
-(require 'mode-setting)
+(load-theme 'solarized-light t)
+;(require 'mode-setting)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; el-get is used by mac and linux,package is used by all.  ;;
 ;; Enable specific plugin in the el-setting.el              ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'el-setting)
+;(require 'el-setting)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; keybind is the last to load ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'keybind-setting)
+;(require 'keybind-setting)
