b8063b78b0093bafa90c54aba439ffffa7c7f7c2
Haoran Feng <haoran.feng@sap.com>
2015-06-13 00:26:25 +0800

    whack space cause the Out of range error

--- config/helpfunc-setting.el
+++ config/helpfunc-setting.el
@@ -23,22 +23,6 @@
   )
 
 
-(defun whack-whitespace (arg)
-  "Delete all white space from point to the next word.  With prefix ARG
-    delete across newlines as well.  The only danger in this is that you
-    don't have to actually be at the end of a word to make it work.  It
-    skips over to the next whitespace and then whacks it all to the next
-    word."
-  (interactive "P")
-  (let ((regexp (if arg "[ \t\n]+" "[ \t]+")))
-    (re-search-forward regexp nil t)
-    (replace-match "" nil nil)))
-
-;; Translate the problematic keys to the function key Hyper,
-;; then bind this to the desired ctrl-i behavior
-(keyboard-translate ?\C-i ?\H-i)
-(global-set-key [?\H-i] 'whack-whitespace)
-
 (defun shell-mode-auto-rename-buffer (text)
   (if (eq major-mode 'shell-mode)
       (rename-buffer  (concat "*Shell: "
