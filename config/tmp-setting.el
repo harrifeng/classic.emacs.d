3e6fd134551928e62c4d1d108c424b0af8d30346
Feng <haoran.feng@sap.com>
2015-09-28 11:57:41 +0800

    rust and go configuration, test them for some time

--- config/tmp-setting.el
+++ config/tmp-setting.el
@@ -15,3 +15,25 @@
 (setq multi-term-program "/bin/bash")
 ;; Use Emacs terminfo, not system terminfo
 (setq system-uses-terminfo nil)
+
+
+(defun rust-save-compile-and-run ()
+  (interactive)
+  (save-buffer)
+
+  (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
+
+      (compile "cargo run")
+
+    (compile
+     (format "rustc %s && %s"
+         (buffer-file-name)
+         (file-name-sans-extension (buffer-file-name))))))
+
+(add-hook 'rust-mode-hook
+      (lambda ()
+        (define-key rust-mode-map (kbd "<f9>") 'rust-save-compile-and-run)))
+
+(add-hook 'go-mode-hook
+      (lambda ()
+        (define-key go-mode-map (kbd "<f9>") 'go-run)))
