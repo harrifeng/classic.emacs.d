607596690ba706d2ceb722db240440dca3808b23
Feng <haoran.feng@sap.com>
2015-10-06 18:00:38 +0800

    To make java run works! Please remember to use M-x ielm

--- config/tmp-setting.el
+++ config/tmp-setting.el
@@ -18,4 +18,22 @@
 
 
 
+(defun java-save-compile-and-run()
+  (interactive)
+  (save-buffer)
+  (compile
+   (format "cd $(git rev-parse --show-cdup) && export TERM=dumb && ./gradlew run -DmainClass=%s"
+           (mapconcat 'identity
+                      (split-string
+                       (substring (file-name-sans-extension (buffer-file-name))
+                                  (string-match "org" (file-name-sans-extension
+                                                       (buffer-file-name)))
+                                  (string-width (file-name-sans-extension
+                                                 (buffer-file-name)))) "/") "."))))
+
+(add-hook 'java-mode-hook
+          (lambda ()
+            (define-key java-mode-map (kbd "<f9>") 'java-save-compile-and-run)
+            (define-key java-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+            ))
 
