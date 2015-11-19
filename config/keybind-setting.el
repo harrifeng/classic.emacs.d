303ffa876541c7830fac5d2d4a24fdfe73911576
Feng <haoran.feng@sap.com>
2015-11-18 21:00:51 -0800

    ok now to be able to swith windows use ctrl +c

--- config/keybind-setting.el
+++ config/keybind-setting.el
@@ -27,3 +27,8 @@
 (global-set-key (kbd "<C-return>")   'mark-whole-buffer)
 (global-set-key (kbd "C-,")          'set-mark-command)
 (global-set-key (kbd "C-2")          'set-mark-command)
+
+(global-set-key (kbd "C-c h")      'windmove-left)
+(global-set-key (kbd "C-c l")      'windmove-right)
+(global-set-key (kbd "C-c j")      'windmove-down)
+(global-set-key (kbd "C-c k")      'windmove-up)
