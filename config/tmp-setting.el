90076b64a7e15f8867d3bf5375ac414925c64593
i309511 <harrifeng@gmail.com>
2015-12-05 22:39:38 -0800

    compilation mode now can see term color

--- config/tmp-setting.el
+++ config/tmp-setting.el
@@ -111,3 +111,11 @@
         ))
 
 ;; objc-mode
+
+
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
